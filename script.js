17d88127750a04a2916e4aaba5a23cf420b53be1
Jawad Dridi <jd_dridi@hotmail.co.uk>
2024-01-14 16:14:28 +0000

    Created a function that plays one round of the game between computer and player

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     
-    
-
     if (randomNumber === 0) {
         return "rock";
     } else if (randomNumber === 1) {
@@ -12,4 +10,27 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice(2));
+//player makes a choice
+//computer makes a choice
+//either player of computer wins
+//if the game is a draw replay the round 
+
+function playGame(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "paper") {
+        return "You loose! Paper beats rock.";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        return "You win! Rock beats Scissors.";
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        return "You loose! Scissors beats paper.";
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        return "You win! Scissors beats paper.";
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        return "You loose! Rock beats scissors.";
+    } else if (playerChoice === computerChoice) {
+        return "Its a draw!";
+    }
+}
+
+console.log(playGame("paper", "paper"));
