6b7466bb377f0829687cb018659e8c62d31fc041
Jawad Dridi <jd_dridi@hotmail.co.uk>
2024-01-14 16:46:07 +0000

    Adjusted so that upper and lower case variables dont affect result

--- script.js
+++ script.js
@@ -16,21 +16,25 @@ function getComputerChoice() {
 //if the game is a draw replay the round 
 
 function playGame(playerChoice, computerChoice) {
-    if (playerChoice === "rock" && computerChoice === "paper") {
-        return "You loose! Paper beats rock.";
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+
+    let playerChoiceLower = playerChoice.toLowerCase();
+    let computerChoiceLower = computerChoice.toLowerCase();
+
+    if (playerChoiceLower === "rock" && computerChoiceLower === "paper") {
+        return "You lose! Paper beats rock.";
+    } else if (playerChoiceLower === "rock" && computerChoiceLower === "scissors") {
         return "You win! Rock beats Scissors.";
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
+    } else if (playerChoiceLower === "paper" && computerChoiceLower === "rock") {
         return "You win! Paper beats rock.";
-    } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        return "You loose! Scissors beats paper.";
-    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    } else if (playerChoiceLower === "paper" && computerChoiceLower === "scissors") {
+        return "You lose! Scissors beats paper.";
+    } else if (playerChoiceLower === "scissors" && computerChoiceLower === "paper") {
         return "You win! Scissors beats paper.";
-    } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        return "You loose! Rock beats scissors.";
-    } else if (playerChoice === computerChoice) {
+    } else if (playerChoiceLower === "scissors" && computerChoiceLower === "rock") {
+        return "You lose! Rock beats scissors.";
+    } else if (playerChoiceLower === computerChoiceLower) {
         return "Its a draw!";
     }
 }
 
-console.log(playGame("paper", "paper"));
+console.log(playGame("ROCK", "Paper"));
