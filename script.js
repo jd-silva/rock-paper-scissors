869b93ba5c72514774fe85680128493c650e595e
Jawad Dridi <jd_dridi@hotmail.co.uk>
2024-01-15 13:55:43 +0000

    added game function that keeps score of the game. withing the game function added a for loop which limits game to 5 rounds. Also added if else statement to name winner after 5 rounds played.

--- script.js
+++ script.js
@@ -10,12 +10,43 @@ function getComputerChoice() {
     }
 }
 
+//make a new function called (game)
+//this function records the results of the match
+//overall the game must be played to the best of 5 to determine a winner 
+//playGame function must be placed inside new game function
+
+
+
 //player makes a choice
 //computer makes a choice
 //either player of computer wins
 //if the game is a draw replay the round 
 
-function playGame(playerChoice, computerChoice) {
+let playerScore = 0;
+let computerScore = 0;
+
+
+function game() {
+    
+    for (let round = 1; round <= 5; round++) {
+        console.log("Round " + round);
+        let result= playGame();
+        console.log(result);
+    }
+
+    // After 5 rounds, determine the winner
+    if (playerScore > computerScore) {
+        console.log("You win the game!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer wins the game!");
+    } else {
+        console.log("It's a draw! No overall winner.");
+    }
+
+
+
+
+function playGame() {
 
 
         let playerChoice = prompt("Enter rock, paper or scissors");
@@ -27,26 +58,25 @@ function playGame(playerChoice, computerChoice) {
     let computerChoiceLower = computerChoice.toLowerCase();
 
     if (playerChoiceLower === "rock" && computerChoiceLower === "paper") {
-        return "You lose! Paper beats rock.";
+        return "You lose! Paper beats rock. Player Score:" + (playerScore) + " ComputerScore:" + (++computerScore);
     } else if (playerChoiceLower === "rock" && computerChoiceLower === "scissors") {
-        return "You win! Rock beats Scissors.";
+        return "You win! Rock beats Scissors. Player Score:" + (++playerScore) + " Computer Score:" + (computerScore);
     } else if (playerChoiceLower === "paper" && computerChoiceLower === "rock") {
-        return "You win! Paper beats rock.";
+        return "You win! Paper beats rock. Player Score:" + (++playerScore) + " Computer Score:" +(computerScore);
     } else if (playerChoiceLower === "paper" && computerChoiceLower === "scissors") {
-        return "You lose! Scissors beats paper.";
+        return "You lose! Scissors beats paper. Player Score:" + (playerScore) + " Computer Score:" + (++computerScore);
     } else if (playerChoiceLower === "scissors" && computerChoiceLower === "paper") {
-        return "You win! Scissors beats paper.";
+        return "You win! Scissors beats paper. Player Score:" + (++playerScore) + " Computer Score:" + (computerScore);
     } else if (playerChoiceLower === "scissors" && computerChoiceLower === "rock") {
-        return "You lose! Rock beats scissors.";
+        return "You lose! Rock beats scissors. PLayer Score:" + (playerScore) + "Computer Score:" + (++computerScore);
     } else if (playerChoiceLower === computerChoiceLower) {
         console.log("It's a draw! Play again.");
     } 
 } else {
     alert ("Invalid choice. Enter rock, paper or scissors.");
-    continue;
     }
 
 
+ }
 }
-
-playGame();
+game()
